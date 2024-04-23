d448eb35898f87ac163dea9488b0626e0480e481
Sergij-alex <lukishin5928@gmail.com>
2024-04-23 19:39:48 +0300

    corrected

--- src/main/java/web/Dao/CarsDao.java
+++ src/main/java/web/Dao/CarsDao.java
@@ -5,10 +5,28 @@ import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListResourceBundle;
 
 @Component
 public class CarsDao {
 
+//    public List<Car> getAllCars() {
+//        Car car = new Car("Audi", "green", 123);
+//        Car car1 = new Car("Toyota", "white", 1234);
+//        Car car2 = new Car("Cherokee", "black", 12345);
+//        Car car3 = new Car("Zhiguli", "yellow", 123456);
+//        Car car4 = new Car("Chevrolet", "red", 1234567);
+//
+//        List<Car> cars = new ArrayList<>();
+//        cars.add(car);
+//        cars.add(car1);
+//        cars.add(car2);
+//        cars.add(car3);
+//        cars.add(car4);
+//
+//        return cars;
+//    }
+//}
     private List<Car> cars;
 
     {
