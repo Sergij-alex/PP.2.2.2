c5528bdf2c9dde5c34d820f0672c1bd1e9683997
Sergij-alex <lukishin5928@gmail.com>
2024-04-23 11:34:23 +0300

    corrected

---
+++ src/main/java/web/Dao/CarsDao.java
@@ -0,0 +1,28 @@
+package web.Dao;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarsDao {
+
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+
+        cars.add(new Car("Audi", "green", 123));
+        cars.add(new Car("Toyota", "white", 1234));
+        cars.add(new Car("Cherrocke", "black", 12345));
+        cars.add(new Car("Zhiguli", "yellow", 123456));
+        cars.add(new Car("Chevralet", "red", 1234567));
+
+    }
+
+    public List<Car> getAllCars() {
+        return cars;
+    }
+}
--- src/main/java/web/servise/CarServiseImpl.java
+++ src/main/java/web/servise/CarServiseImpl.java
@@ -1,43 +1,35 @@
 package web.servise;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import web.Dao.CarsDao;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarServiseImpl implements CarServise {
 
+    private final CarsDao carsDao;
+
+    @Autowired
+    public CarServiseImpl(CarsDao carsDao) {
+        this.carsDao = carsDao;
+    }
+
     @Override
     public List<Car> getCars(int a) {
-        Car car = new Car("Audi", "green", 123);
-        Car car1 = new Car("Toyota", "white", 1234);
-        Car car2 = new Car("Cherrocke", "black", 12345);
-        Car car3 = new Car("Zhiguli", "yellow", 123456);
-        Car car4 = new Car("Chevralet", "red", 1234567);
-        List<Car> cars = new ArrayList<>();
-        cars.add(car);
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-        cars.add(car4);
+        List<Car> cars = carsDao.getAllCars();
 
-        if (a >= 5) {
-            a = 5;
+        if (a >= cars.size()) {
+            a = cars.size();
         }
         if (a < 0) {
             a = 0;
         }
-        List<Car> cars1 = new ArrayList<>();
 
-        for(int i = 0; i < a; i++) {
-            cars1.add(cars.get(i));
-        }
-        return cars1;
+        List<Car> selectedCars = cars.subList(0, a);
+
+        return selectedCars;
     }
 }
-
-
-
